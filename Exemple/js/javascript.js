c5d3a488445f246ac3e1c33c190e737381d74aa4
Zunay88 <alvarobcn88@hotmail.com>
2018-02-06 10:34:31 +0100

    Update

---
+++ Exemple/js/javascript.js
@@ -0,0 +1,8 @@
+function MarioLuigi(){
+	var imatge = document.getElementById('personatge');
+	if (imatge.src.match("img/mario.png")){
+		imatge.src = "img/luigi.png"
+	}else{
+		imatge.src = "img/mario.png"
+	}
+}
